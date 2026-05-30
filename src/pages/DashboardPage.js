e2caf41f4c1645533869a3fbea5be48ea878f20e
Cassiel31 <mscids2024.cassiel@unigoa.ac.in>
2026-05-30 12:08:13 +0530

    Update DashboardPage.js

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -19,103 +19,111 @@ export default function DashboardPage({
         fontFamily: "Inter, sans-serif",
         padding: "24px",
         boxSizing: "border-box",
-        width: "100%",
       }}
     >
       <div
         style={{
+          marginBottom: "36px",
+        }}
+      >
+        <p
+          style={{
+            color: "#8b8b8b",
+            marginBottom: "8px",
+            fontSize: "16px",
+          }}
+        >
+          Hello,
+        </p>
+
+        <h1
+          style={{
+            fontFamily:
+              "Playfair Display, serif",
+            fontSize: "52px",
+            lineHeight: "1",
+            margin: 0,
+            color: "#1f1f23",
+            wordBreak: "break-word",
+          }}
+        >
+          {currentUser?.full_name}
+        </h1>
+
+        <p
+          style={{
+            marginTop: "12px",
+            color: "#777",
+            fontSize: "16px",
+          }}
+        >
+          @{currentUser?.username}
+        </p>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "18px",
           width: "100%",
         }}
       >
-        <div
-          style={{
-            marginBottom: "36px",
-          }}
-        >
-          <p
-            style={{
-              color: "#8b8b8b",
-              marginBottom: "10px",
-              fontSize: "18px",
-            }}
-          >
-            Hello,
-          </p>
+        <DashboardCard
+          icon={
+            <CalendarCheck
+              size={42}
+            />
+          }
+          title="Attendance"
+          onClick={() =>
+            setCurrentPage(
+              "attendance"
+            )
+          }
+        />
 
-          <h1
-            style={{
-              fontFamily:
-                "Playfair Display, serif",
-              fontSize: "56px",
-              lineHeight: "1",
-              margin: 0,
-              color: "#1f1f23",
-              wordBreak: "break-word",
-            }}
-          >
-            {currentUser?.full_name}
-          </h1>
+        <DashboardCard
+          icon={
+            <User
+              size={42}
+            />
+          }
+          title="Personal Details"
+          onClick={() =>
+            setCurrentPage(
+              "personal"
+            )
+          }
+        />
 
-          <p
-            style={{
-              marginTop: "12px",
-              color: "#777",
-              fontSize: "18px",
-            }}
-          >
-            @{currentUser?.username}
-          </p>
-        </div>
+        <DashboardCard
+          icon={
+            <Crown
+              size={42}
+            />
+          }
+          title="Hierarchy"
+          onClick={() =>
+            setCurrentPage(
+              "hierarchy"
+            )
+          }
+        />
 
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns:
-              "1fr 1fr",
-            gap: "20px",
-            width: "100%",
-          }}
-        >
-          <DashboardCard
-            icon={<CalendarCheck size={52} />}
-            title="Attendance"
-            onClick={() =>
-              setCurrentPage(
-                "attendance"
-              )
-            }
-          />
-
-          <DashboardCard
-            icon={<User size={52} />}
-            title="Personal Details"
-            onClick={() =>
-              setCurrentPage(
-                "personal"
-              )
-            }
-          />
-
-          <DashboardCard
-            icon={<Crown size={52} />}
-            title="Hierarchy"
-            onClick={() =>
-              setCurrentPage(
-                "hierarchy"
-              )
-            }
-          />
-
-          <DashboardCard
-            icon={<Building2 size={52} />}
-            title="SNSD Updates"
-            onClick={() =>
-              setCurrentPage(
-                "snsd"
-              )
-            }
-          />
-        </div>
+        <DashboardCard
+          icon={
+            <Building2
+              size={42}
+            />
+          }
+          title="SNSD Updates"
+          onClick={() =>
+            setCurrentPage(
+              "snsd"
+            )
+          }
+        />
       </div>
     </div>
   );
@@ -130,41 +138,52 @@ function DashboardCard({
     <button
       onClick={onClick}
       style={{
+        width: "100%",
+
+        height: "140px",
+
         border: "none",
-        borderRadius: "32px",
-        backgroundColor: "white",
+
+        borderRadius:
+          "30px",
+
+        backgroundColor:
+          "white",
+
         boxShadow:
           "0 10px 30px rgba(0,0,0,0.05)",
+
         cursor: "pointer",
 
-        height: "220px",
-
         display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
 
-        padding: "20px",
+        alignItems:
+          "center",
+
+        gap: "22px",
+
+        padding: "24px",
 
         boxSizing:
           "border-box",
 
-        width: "100%",
+        textAlign: "left",
       }}
     >
       <div
         style={{
-          height: "60px",
+          width: "60px",
 
           display: "flex",
-
-          alignItems:
-            "center",
 
           justifyContent:
             "center",
 
-          marginBottom: "20px",
+          alignItems:
+            "center",
+
+          color:
+            "#1f1f23",
         }}
       >
         {icon}
@@ -172,26 +191,16 @@ function DashboardCard({
 
       <div
         style={{
-          fontSize: "22px",
+          fontSize: "24px",
 
-          fontWeight: "600",
+          fontWeight:
+            "600",
 
-          color: "#1f1f23",
+          color:
+            "#1f1f23",
 
-          textAlign:
-            "center",
-
-          lineHeight: "1.25",
-
-          minHeight: "60px",
-
-          display: "flex",
-
-          alignItems:
-            "center",
-
-          justifyContent:
-            "center",
+          fontFamily:
+            "Playfair Display, serif",
         }}
       >
         {title}
